1820190dd6784249b02b26f48fabd0028e2080fb
Zerefu0exe <64784378+Zerefu0exe@users.noreply.github.com>
2021-02-14 22:47:28 +0300

    Stable version (#1)

---
+++ TestLib/TestLib.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MathAnalyzerSpace;
+
+namespace TestLib
+{
+    internal class TestLib
+    {
+        private static void Main() //string[] args
+        {
+            //string str = "(42+1/(42+1))/(3+2*41/(42+2))"; // test string
+            string str = "5+9*9-10";
+
+            Console.WriteLine(str);
+            Console.WriteLine(MathAnalyzer.Delimiter(str));
+            Console.WriteLine(MathAnalyzer.Solution(str));
+        }
+    }
+}
